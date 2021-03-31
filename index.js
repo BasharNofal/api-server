59aa3ac5ba34c48c8e98bdcba81559caf5812526
BasharNofal <bashar.nofal1997@gmail.com>
2021-03-31 20:30:10 +0300

    i have error in deploment and error in testing

--- index.js
+++ index.js
@@ -6,4 +6,13 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
     require('./src/server.js').start(process.env.PORT);
 }).catch(error => {
     console.log('An error occurred while connecting to database', error);
-})
+});
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://bashar:9971011997@cluster0.axhe7.mongodb.net/api?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
